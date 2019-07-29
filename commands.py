3df8ac677e722d15b7283b5a138fb9adfd3bf57f
mfboulos <mboulos95@gmail.com>
2019-07-29 12:56:42 -0400

    Updated Command classes with better implementations and direct access to a bot instance

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
-from enum import Enum, auto
 from datetime import datetime, timedelta
+from enum import Enum, auto
+from bot import TwitchBot
 
 class Permission(Enum):
     USER = auto()
@@ -8,23 +9,60 @@ class Permission(Enum):
     ADMIN = auto()
 
 class Command(object):
-    def __init__(self, permission=Permission.USER):
+    def __init__(self,
+                 bot: TwitchBot,
+                 permission=Permission.USER):
+        self._bot = bot
         self.permission = permission
+    
+    def run(self, message):
+        """
+        Run the command based on the message.
+
+        Subclasses should override this function.
+        """
+
+        pass
+    
 
 class ExecutedCommand(Command):
-    def __init__(self, command, permission=Permission.USER):
-        super().__init__(permission)
-        self.command = command
+    def __init__(self,
+                 bot: TwitchBot,
+                 name,
+                 response=None,
+                 permission=Permission.USER):
+        super().__init__(bot, permission=permission)
+        self.name = name
+        self.response = response
     
     def _parse_message(self, message):
+        """
+        Parse a message into what would be read as the execution token and its
+        arguments separately.
+
+        Returns a tuple pair of the potential execution token and its list of
+        respective arguments.
+        """
+
         tokens = message.split()
         return (tokens[0], tokens[1:])
     
-    def match(self, message):
-        return self._parse_message(message)[0] is self.command
+    def _match(self, message):
+        """
+        Returns `True` if the message's execution token matches the name of the
+        command, `False` otherwise.
+        """
+
+        return self._parse_message(message)[0] is self.name
     
-    def run(self, tokens):
-        pass
+    def run(self, message):
+        """
+        Processes the message first, then makes the bot say this command's
+        response in chat if the message matches the command.
+        """
+
+        if self._match(message) and self.response:
+            self._bot.write(self.response)
 
 class TimedCommand(Command):
     def __init__(self,
@@ -35,18 +73,27 @@ class TimedCommand(Command):
         super().__init__(permission)
         self.min_messages = min_messages
         self.interval = interval
-        self._message = message
+        self.message = message
 
-        self._num_messages = 0
+        self.__num_messages = 0
         self.base_time = datetime.now()
     
+    @property
     def _ready(self):
-        message_check = self._num_messages >= self.min_messages
+        """
+        Returns `True` if this command is ready to execute again, `False` otherwise
+        """
+
+        message_check = self.__num_messages >= self.min_messages
         time_check = datetime.now() - self.base_time > timedelta(seconds=self.interval)
         return message_check and time_check
     
-    def process_message(self):
-        self._num_messages += 1
-    
-    def get_message(self):
-        return self._message if self._ready() else None
+    def run(self):
+        """
+        Makes the bot write this command's message if the command is ready
+        """
+
+        self.__num_messages += 1
+        if self._ready:
+            self._bot.write(self.message)
+            self.__num_messages = 0
