4989426f9318651cee914b3bd97808ae77f7d753
mfboulos <mboulos95@gmail.com>
2019-07-29 12:11:04 -0400

    Updated config, fixed syntax issue

--- bot_config.py
+++ bot_config.py
@@ -8,18 +8,18 @@ class TwitchBotConfig(object):
     """
 
     def __init__(self,
-                 channel = '#' + os.environ['TWITCH_USER'],
+                 channel = os.environ['TWITCH_USER'],
                  password = os.environ['TWITCH_OAUTH'],
-                 nickname = channel + '_bot',
+                 nickname = None,
                  ignore_patterns = ['nightbot', 'moobot', '.+_bot']):
         self.channel = channel
         self.password = password
-        self.nickname = nickname
+        self.nickname = nickname if nickname else channel[1:] + '_bot'
         self.ignore_patterns = ignore_patterns
 
 class DiceBotConfig(TwitchBotConfig):
     def __init__(self,
-                 channel = '#' + os.environ['TWITCH_USER'],
+                 channel = os.environ['TWITCH_USER'],
                  password = os.environ['TWITCH_OAUTH'],
-                 ignore_patterns = ['nightbot', 'moobot', '.+_bot'])):
+                 ignore_patterns = ['nightbot', 'moobot', '.+_bot']):
         super().__init__(channel, password, 'DiceBot', ignore_patterns)
